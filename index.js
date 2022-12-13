83004039c7d33b23e8a3f498531beb549b4cf10e
Tobias Silvestri <tobi.silvestri1@gmail.com>
2022-12-13 19:11:01 -0300

    convertidor de moneda

---
+++ index.js
@@ -0,0 +1,18 @@
+function convertir() {
+    var valore = parseInt(document.getElementById("valor").value);
+    var resultado = 0;
+    var dolar = 320;
+    var euro = 325;
+    if (document.getElementById("uno").checked){
+        resultado = valore / dolar;
+        alert("el cambio de Pesos a Dolar es: $" + resultado.toFixed(2))
+    }
+    else if (document.getElementById("dos").checked){
+        resultado = valore / euro;
+        alert("el cambio de pesos a euros es: $" + resultado.toFixed(2));
+    }
+    else{
+        alert("tienes que completar todos los requerimientos")
+    }
+    
+}
